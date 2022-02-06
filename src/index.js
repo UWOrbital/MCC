259adea1558e4b0eaf9b65eedff3b7cf6d25ddc0
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2022-02-06 01:11:57 -0500

    Add Firebase support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbfXcM1J1u8BVTK3CqxmcUFMvRWDylViY",
+  authDomain: "uw-orbital-mcc.firebaseapp.com",
+  projectId: "uw-orbital-mcc",
+  storageBucket: "uw-orbital-mcc.appspot.com",
+  messagingSenderId: "784474228563",
+  appId: "1:784474228563:web:5b4eed8840a8b286cd83a6",
+  measurementId: "G-JCYQQ2GLKV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
